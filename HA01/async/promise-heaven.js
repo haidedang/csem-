afb7ac8831223d865f99469750394dc2f15c5a1f
simonhuber95 <33151777+simonhuber95@users.noreply.github.com>
2017-10-30 18:39:17 +0100

    Finished Promise Heaven

--- HA01/async/promise-heaven.js
+++ HA01/async/promise-heaven.js
@@ -11,31 +11,39 @@ function randomError() {
 let welcome = new Promise((resolve, reject)=>{
   err = randomError();
   if (err) {
-    console.error('Oh no!', err); return;
+    reject('Oh no! ' + err); 
+    return;
     }
-    return console.log('Welcome');
+    resolve('Welcome');
 });
 
-let to = new Promise((resolve, reject)=>{
-  err = randomError();  
-    if (err) {
-    console.error('Wut?', err); return;
-    }
-    return console.log(' to');
-});
+const to = (string) => {
+  return new Promise((resolve, reject) => {
+      err = randomError();  
+      if (err) {
+        reject('Wut? ' + err); 
+        return;
+      }
+      resolve(string + ' to');
+  })
+}
 
-let paradise = new Promise((resolve, reject)=>{
-  err = randomError();    
-    if (err) {
-    console.error('Srsly?', err); return;
-    }
-    return console.log(' paradise \u2601 \u2601');
-});
+  
+const paradise = (string) => {
+  return new Promise((resolve, reject) => {
+      err = randomError();  
+      if (err) {
+        reject('Srsly? ' + err) 
+        return;
+      }
+      resolve(string + ' paradise \u2601 \u2601');
+  })
+}
 
-// TODO: write the three missing Promises and assign them to the variables
-// "welcome", "to", and "paradise"
+// DONE
 
-welcome.then(to).then(paradise).then(data => console.log(data)).catch(err => console.error(err));
+
+welcome.then(to).then(paradise).then(data => console.log(data)).catch(err => console.error(err))
 
 // Example outputs:
 // Srsly? A random error occured
